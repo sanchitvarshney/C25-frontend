67bfbd0f89f2b18cccdf5a221bd3287b4d5f8bf1
deepakMS12 <deepak.dhiman@mscorpres.in>
2026-07-06 14:09:45 +0530

    upload doc api  split

--- src/Pages/Store/MaterialIn/MaterialInWithoutPO/MaterialInWithoutPO.jsx
+++ src/Pages/Store/MaterialIn/MaterialInWithoutPO/MaterialInWithoutPO.jsx
@@ -215,7 +215,7 @@ const [fileName, setFileName] = useState("");
         materialResetFunction();
         setPreviewRows([]);
         setPreview(false);
-
+        setFileName("");
         setSubmitLoading(false);
       } else {
         showToast(response.message, "error");
